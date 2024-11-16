e2049bef5de6c0a9fec050dd701ad647f77415c7
Manuel Cardoso <manuelcardosomasc@gmail.com>
2024-11-16 17:48:23 +0100

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import logoImg from "../public/logo.svg";
+import iconFolder from "../public/icon-folder.svg";
+import iconUpload from "../public/icon-upload.svg";
+import iconDocument from "../public/icon-document.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div id="container">
+      <section className="left_side section-area">
+        <img src={logoImg} alt="Logotipo" />
+        <figure>
+          <img src={iconDocument} alt="" />
+          <img src={iconFolder} alt="" />
+          <img src={iconUpload} alt="" />
+        </figure>
+      </section>
+      <section className="right_side section-area">
+        <p> You’ve used 815 GB of your storage</p>
+        <div className="progress_bar">
+          <div className="progress_bar_clr">
+            <div className="progress_bar_btn"></div>
+          </div>
+        </div>
+        <div className="progress_bar_legend">
+          <p>0 GB</p>
+          <p>1000 GB</p>
+        </div>
+        <div className="show_space">
+          <p>185</p>
+          <p>Gb left</p>
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default App
+export default App;
